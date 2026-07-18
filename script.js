adc43e9ae837e4bf71d459225738886a1ebc70f8
kardamn009-commits <kardamn009@gmail.com>
2026-07-18 10:25:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,50 @@
+body{
+    margin:0;
+    font-family:Arial,sans-serif;
+    background:#f5f5f5;
+}
 
+header{
+    background:#2563EB;
+    color:white;
+    text-align:center;
+    padding:40px 20px;
+}
+
+nav{
+    background:#111827;
+    padding:15px;
+    text-align:center;
+}
+
+nav a{
+    color:white;
+    text-decoration:none;
+    margin:15px;
+    font-weight:bold;
+}
+
+nav a:hover{
+    color:#60A5FA;
+}
+
+section{
+    padding:50px 20px;
+    text-align:center;
+}
+
+h2{
+    color:#2563EB;
+}
+
+p{
+    font-size:18px;
+}
+
+footer{
+    background:#111827;
+    color:white;
+    text-align:center;
+    padding:20px;
+    margin-top:40px;
+}
