f8398abce8402ff90cf4cbcccb16867d773e4f9f
kardamn009-commits <kardamn009@gmail.com>
2026-07-21 09:56:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,3 +9,14 @@ const observer = new IntersectionObserver((entries) => {
 document.querySelectorAll(".fade-up").forEach((el) => {
     observer.observe(el);
 });
+const darkBtn = document.getElementById("darkModeBtn");
+
+darkBtn.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+
+    if(document.body.classList.contains("dark-mode")){
+        darkBtn.innerHTML = "☀️ Light Mode";
+    }else{
+        darkBtn.innerHTML = "🌙 Dark Mode";
+    }
+});
