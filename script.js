fc69b0b9267d3b665914d43b3dcb2daa7f52763e
kardamn009-commits <kardamn009@gmail.com>
2026-07-21 08:59:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-const observer = new IntersectionObserver((entries)=>{
-    entries.forEach(entry=>{
-        if(entry.isIntersecting){
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
             entry.target.classList.add("show");
         }
     });
 });
 
-document.querySelectorAll(".fade-up").forEach((el)=>{
+document.querySelectorAll(".fade-up").forEach((el) => {
     observer.observe(el);
 });
